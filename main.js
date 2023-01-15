7ba1000d80ba086ab5ba6dcbbb724b70cb371320
Arinze Timothy <78275394+TimzonaTimothy@users.noreply.github.com>
2023-01-15 21:47:47 +0100

    my regex

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-let sample = "Hello world";
+let sample = "4000000000099";
 
-let my_regex = /hello/i;
+const my_regex = /^4[0-9]{12}(?:[0-9]{3})?$/;
+
+// where
+// ^ represents the starting of the string.
+// 4 represents the string that should start with 4.
+// [0-9]{12} represents the next twelve digits should be any between 0-9.
+// ( represents the start of the group.
+// ? represents the 0 or 1 time.
+// [0-9]{3} represents the next three digits should be any between 0-9.
+// ) represents the ending of the group.
+// ? represents the 0 or 1 time.
+// $ represents the ending of the string.
+
 let result = my_regex.test(sample)
 console.log(result);
+
